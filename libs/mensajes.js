fdc871d76baafcc2338401f2051f5e1e5d466a27
Isaac-Ochoa <isaacob.ti23@utsjr.edu.mx>
2025-02-09 23:01:48 -0600

    Update mensajes.js

--- libs/mensajes.js
+++ libs/mensajes.js
@@ -1,4 +1,4 @@
-export function mensajes(status, mensajesUsuario, mensajeOriginal=""){
+export function mensajes(status, mensajesUsuario, mensajeOriginal=""){ //se agregan las rutas de los mensajes
     return{
         status,
         mensajesUsuario,
